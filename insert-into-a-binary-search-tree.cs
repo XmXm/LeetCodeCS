1feb36f26a5e7a277bb5fb007ba1db13c90cb4ee
zyx <zy_s_h@me.com>
2020-08-22 12:59:00 +0800

    add readme

---
+++ insert-into-a-binary-search-tree.cs
@@ -0,0 +1,11 @@
+using System;
+/// <summary>
+/// https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
+/// </summary>
+public partial class Solution
+{
+    public TreeNode InsertIntoBST(TreeNode root, int val)
+    {
+        throw new NotImplementedException();
+    }
+}
